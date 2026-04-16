c2375a3c255b10593db00dd9f9b5f820969ba6d2
Kelvin Kiarie <47083652+kelvinsky254@users.noreply.github.com>
2026-04-16 12:50:27 -0400

    Fixing the wsgi.py to reflect new structural changes

--- parkwise/wsgi.py
+++ parkwise/wsgi.py
@@ -11,6 +11,6 @@ import os
 
 from django.core.wsgi import get_wsgi_application
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'parkwise.settings')
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'parkwise.settings.prod')
 
 application = get_wsgi_application()
