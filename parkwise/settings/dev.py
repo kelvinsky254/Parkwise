1b9a4accff650d9984861ade7a5193edd04d1a04
kelvinsky254 <kelvin.kiarie96@gmail.com>
2026-04-18 18:06:22 -0400

    fix: resolve PostgreSQL port conflict with local Windows install

--- parkwise/settings/dev.py
+++ parkwise/settings/dev.py
@@ -1,7 +1,19 @@
 from .base import * #noqa
+from decouple import config
 
 DEBUG = True
 
 ALLOWED_HOSTS = ["*"]
 
-EMAIL_BACKEND = "django.core.mail.backend.console.EmailBackend"
+EMAIL_BACKEND = "django.core.mail.backends.console.EmailBackend"
+
+DATABASES = {
+    "default":{
+        "ENGINE": "django.db.backends.postgresql",
+        "NAME": config("DB_NAME", default="parkwise"),
+        "USER": config("DB_USER", default="parkwise"),
+        "PASSWORD": config("DB_PASSWORD", default="parkwise"),
+        "HOST": config("DB_HOST", default="localhost"),
+        "PORT": config("DB_PORT", default="5432"),
+    }
+}
