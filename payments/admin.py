69aec14f95a64f72bc87d0755e24d1a2f6dbfedf
kelvinsky254 <kelvin.kiarie96@gmail.com>
2026-04-20 23:58:11 -0400

    feat(payments): register PaymentSessionAdmin in admin

--- payments/admin.py
+++ payments/admin.py
@@ -1,3 +1,11 @@
 from django.contrib import admin
+from .models import PaymentSession
 
-# Register your models here.
+
+@admin.register(PaymentSession)
+class PaymentSessionAdmin(admin.ModelAdmin):
+    list_display = ("plate_number", "phone_number", "lot", "vendor", "status", "created_at")
+    search_fields = ("plate_number", "phone_number")
+    list_filter = ("status",)
+    list_select_related = ("lot", "vendor", "user")
+    readonly_fields = ("ussd_string", "tel_uri", "plate_number", "phone_number")
