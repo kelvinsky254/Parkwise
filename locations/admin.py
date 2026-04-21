490ca3e8f3fc1eef5b2b2e68d816987b920b205c
kelvinsky254 <kelvin.kiarie96@gmail.com>
2026-04-20 23:56:00 -0400

    feat(locations): register ParkingLot and LotVendorMapping in admin

--- locations/admin.py
+++ locations/admin.py
@@ -1,3 +1,16 @@
 from django.contrib import admin
+from .models import ParkingLot, LotVendorMapping
 
-# Register your models here.
+
+@admin.register(ParkingLot)
+class ParkingLotAdmin(admin.ModelAdmin):
+    list_display = ("name", "address", "latitude", "longitude", "is_active")
+    search_fields = ("name", "address")
+    list_filter = ("is_active",)
+
+
+@admin.register(LotVendorMapping)
+class LotVendorMappingAdmin(admin.ModelAdmin):
+    list_display = ("lot", "vendor", "effective_from", "effective_to", "is_active")
+    list_select_related = ("lot", "vendor")
+    list_filter = ("is_active",)
